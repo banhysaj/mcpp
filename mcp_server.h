af2727d8302771d99c61098d5c943b9cfa7d3b64
banHysaj <albanhysaj20@outlook.com>
2026-09-12 18:42:12 +0200

    Add server base, minor

---
+++ mcp_server.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <string>
+#include "mcp_types.h"
+#include <functional>
+
+namespace mcpp {
+
+	class Server; //Forward declaration
+
+	class Server
+	{
+	public:
+		Server(const std::string& name, const std::string& version);
+		~Server();
+
+		const std::string& name() const { return name_; }
+		const std::string& version() const { return version_; }
+
+	private:
+
+		std::string name_;
+		std::string version_;
+		bool initialized_;
+	};
+}
